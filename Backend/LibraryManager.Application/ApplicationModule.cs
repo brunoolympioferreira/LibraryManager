51b460d0e8000f66248c461352db6c8348996ff9
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-01-10 17:38:05 -0300

    implementando bootstrap modules

---
+++ Backend/LibraryManager.Application/ApplicationModule.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryManager.Application;
+public static class ApplicationModule
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        AddServices(services);
+    }
+
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        return services;
+    }
+
+}
---
+++ Backend/LibraryManager.Infraestructure/InfraestructureModule.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager.Infraestructure;
+public static class InfraestructureModule
+{
+    public static void AddInfraestructure(this IServiceCollection services)
+    {
+    }
+
+    private static IServiceCollection AddDb(this IServiceCollection services, string connectionString)
+    {
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        return services;
+    }
+}
